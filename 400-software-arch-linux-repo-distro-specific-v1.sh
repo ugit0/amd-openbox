97c957acb62a4c20e51e8760fe0f26fce7f16300
Erik Dubois <erik.dubois@gmail.com>
2017-12-01 16:14:11 +0100

    xfce session

--- 400-software-arch-linux-repo-distro-specific-v1.sh
+++ 400-software-arch-linux-repo-distro-specific-v1.sh
@@ -61,6 +61,7 @@ sudo pacman -S xorg-xrandr --noconfirm --needed
 sudo pacman -S xfce4-appfinder --noconfirm --needed
 sudo pacman -S xfce4-clipman-plugin --noconfirm --needed
 sudo pacman -S xfce4-power-manager --noconfirm --needed
+sudo pacman -S xfce4-session-logout --noconfirm --needed
 sudo pacman -S xfce4-settings --noconfirm --needed
 sudo pacman -S xfce4-screenshooter --noconfirm --needed
 sudo pacman -S xfce4-taskmanager --noconfirm --needed
