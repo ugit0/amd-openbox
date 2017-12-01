635525d130f054f5ad0cb6319b77104b22e20fd2
Erik Dubois <erik.dubois@gmail.com>
2017-12-01 14:34:13 +0100

    xfce slimlock and obmenu-generator

--- 500-software-AUR-repo-distro-specific-v1.sh
+++ 500-software-AUR-repo-distro-specific-v1.sh
@@ -13,60 +13,10 @@ set -e
 #
 ##################################################################################################################
 
-echo "DESKTOP SPECIFIC APPLICATIONS"
 
+sh AUR/install-obmenu-generator-aur-v*.sh
 
-echo "################################################################"
-echo "xfce-slimlock"
-echo "################################################################"
-
-package="xfce-slimlock"
-
-#----------------------------------------------------------------------------------
-
-#checking if application is already installed or else install with aur helpers
-if pacman -Qi $package &> /dev/null; then
-
-	echo "################################################################"
-	echo "################## "$package" is already installed"
-	echo "################################################################"
-
-else
-
-	#checking which helper is installed
-	if pacman -Qi packer &> /dev/null; then
-
-		echo "Installing with packer"
-		packer -S --noconfirm --noedit  $package
-
-	elif pacman -Qi pacaur &> /dev/null; then
-
-		echo "Installing with pacaur"
-		pacaur -S --noconfirm --noedit  $package
-
-	elif pacman -Qi yaourt &> /dev/null; then
-
-		echo "Installing with yaourt"
-		yaourt -S --noconfirm $package
-
-	fi
-
-	# Just checking if installation was successful
-	if pacman -Qi $package &> /dev/null; then
-
-	echo "################################################################"
-	echo "#########  "$package" has been installed"
-	echo "################################################################"
-
-	else
-
-	echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-	echo "!!!!!!!!!  "$package" has NOT been installed"
-	echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-
-	fi
-
-fi
+sh AUR/install-xfce-slimlock-aur-v*.sh
 
 
 echo "################################################################"
