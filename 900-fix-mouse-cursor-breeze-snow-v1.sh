c9d3dbc06bbe71d639accf035a9c62a6abd20294
Erik Dubois <erik.dubois@gmail.com>
2017-12-01 19:07:12 +0100

    breeze cursor everywhere

---
+++ 900-fix-mouse-cursor-breeze-snow-v1.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+##################################################################################################################
+# Author 	: 	Erik Dubois
+# Website 	: 	https://www.erikdubois.be
+# Website	:	https://www.archmerge.info
+# Website	:	https://www.archmerge.com
+# Website	:	https://www.archmerged.com
+# Website	:	https://www.archmergeforum.com
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+echo "Everywhere Breeze-snow as cursor"
+echo "Change cursor if you want"
+
+sudo mkdir /usr/share/icons/default
+sudo cp -r Personal/settings/default/index.theme /usr/share/icons/default
+
+
+echo "################################################################"
+echo "####                             Fix done                 ######"
+echo "################################################################"
