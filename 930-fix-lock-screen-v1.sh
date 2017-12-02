166bab54e6800bdfffc1d7d9f3fd179c4f01ddb7
Erik Dubois <erik.dubois@gmail.com>
2017-12-02 13:54:00 +0100

    typo

--- 930-fix-lock-screen-v1.sh
+++ 930-fix-lock-screen-v1.sh
@@ -15,7 +15,7 @@ set -e
 
 echo "Change lock screen to standard lock screen in ArchMerge ..."
 
-sudo sed -i 's/current_theme       default/current_theme       = archmerge_transfer/g' /etc/slim.conf
+sudo sed -i 's/current_theme       default/current_theme       archmerge_transfer/g' /etc/slim.conf
 
 echo "################################################################"
 echo "####               LOCK    SCREEN   APPLIED               ######"
