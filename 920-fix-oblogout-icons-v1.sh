75625831c1e4d3682fc426e209804d7d8c55a012
Erik Dubois <erik.dubois@gmail.com>
2017-12-02 13:47:13 +0100

    oblogout

--- 920-fix-oblogout-icons-v1.sh
+++ 920-fix-oblogout-icons-v1.sh
@@ -15,7 +15,7 @@ set -e
 
 echo "Sardi icons applied when logging out or shutdown or ..."
 
-sudo sed -i 's/'buttontheme = oxygen'/'buttontheme = Sardi-Oblogout'/g' /etc/oblogout.conf
+sudo sed -i 's/buttontheme = oxygen/buttontheme = Sardi-Oblogout/g' /etc/oblogout.conf
 #sudo sed -i 's/'buttontheme = oxygen'/'buttontheme = adeos-branco'/g' /etc/oblogout.conf
 #sudo sed -i 's/'buttontheme = oxygen'/'buttontheme = adeos-branco-mono'/g' /etc/oblogout.conf
 #sudo sed -i 's/'buttontheme = oxygen'/'buttontheme = adeos-branco-cores'/g' /etc/oblogout.conf
