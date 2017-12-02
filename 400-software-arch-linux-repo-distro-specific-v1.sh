66e3c5975067b20cb140a458bbdf34adeb2249d7
Erik Dubois <erik.dubois@gmail.com>
2017-12-02 21:54:26 +0100

    gsimplecal added

--- 400-software-arch-linux-repo-distro-specific-v1.sh
+++ 400-software-arch-linux-repo-distro-specific-v1.sh
@@ -48,6 +48,7 @@ sudo pacman -S compton  --noconfirm --needed
 sudo pacman -S dmenu  --noconfirm --needed
 sudo pacman -S feh --noconfirm --needed
 sudo pacman -S gmrun --noconfirm --needed
+sudo pacman -S gsimplecal --noconfirm --needed
 sudo pacman -S lxappearance --noconfirm --needed
 sudo pacman -S lxrandr --noconfirm --needed
 sudo pacman -S nitrogen --noconfirm --needed
