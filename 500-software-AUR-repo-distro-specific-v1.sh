72502835a1601444234fc1757d0be0fbb131c8ab
Erik Dubois <erik.dubois@gmail.com>
2017-12-03 18:27:21 +0100

    final update

--- 500-software-AUR-repo-distro-specific-v1.sh
+++ 500-software-AUR-repo-distro-specific-v1.sh
@@ -27,6 +27,7 @@ sh AUR-DS/install-perl-linux-desktopfiles-v*.sh
 
 sh AUR-DS/install-xfce-slimlock-v*.sh
 
+
 echo "################################################################"
 echo "####       Software from AUR-DS folder installed          ######"
 echo "################################################################"
